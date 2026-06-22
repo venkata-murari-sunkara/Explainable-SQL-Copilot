085274f43fbcd2ca8bd3b0a50cb200bcc725b8a6
Venkata Murari <venkatamurari@Venkatas-MacBook-Air.local>
2026-06-22 12:54:30 +0530

    Configure frontend API URL

--- frontend/app.py
+++ frontend/app.py
@@ -2,8 +2,12 @@ import streamlit as st
 import requests
 import pandas as pd
 import plotly.express as px
+import os
 
-API_URL = "http://127.0.0.1:8000"
+API_URL = os.getenv(
+    "API_URL",
+    "http://127.0.0.1:8000"
+)
 
 st.title("🤖 Explainable SQL Copilot")
 
